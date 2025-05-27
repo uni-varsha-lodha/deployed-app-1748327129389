45e997d7f0ab72835b133197faacc4a5a006b3b9
uni-varsha-lodha <148821562+uni-varsha-lodha@users.noreply.github.com>
2025-05-26 23:25:35 -0700

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <p className="text-lg">Learn more about our company and mission.</p>
+    </div>
+  );
+}
+
+export default About;
